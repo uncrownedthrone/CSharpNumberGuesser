dc592873f5a6ee85bc607aa71412d5589b52d95c
Justin Swiger <justin.swiger@me.com>
2019-12-11 16:03:10 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpNumberGuesser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
