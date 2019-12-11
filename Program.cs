cc916e446a8d3713034c44bd64c7ac6e9a9dd736
Justin Swiger <justin.swiger@me.com>
2019-12-11 16:05:21 -0500

    changed name

--- Program.cs
+++ Program.cs
@@ -2,11 +2,11 @@
 
 namespace CSharpNumberGuesser
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      Console.WriteLine("Number Guesser");
     }
+  }
 }
